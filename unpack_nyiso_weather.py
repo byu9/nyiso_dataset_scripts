7c8f60d6d55be63e224430dadce4e680b4db269c
John Yu <byu9@ncsu.edu>
2024-08-29 14:16:32 -0400

    add NYISO weather unpack script

---
+++ unpack_nyiso_weather.py
@@ -0,0 +1,8 @@
+from glob import glob
+from zipfile import ZipFile
+
+filenames = glob('fetch_nyiso_weather/*.zip')
+
+for filename in filenames:
+    with ZipFile(filename, 'r') as file:
+        file.extractall('unpack_nyiso_weather')
