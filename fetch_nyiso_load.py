1a18765df59150d7df253c7ea089d67af9360b50
John Yu <byu9@ncsu.edu>
2024-08-29 14:13:14 -0400

    add fetch NYISO load script

---
+++ fetch_nyiso_load.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+
+years = [2020, 2021, 2022, 2023]
+months = list(range(1, 13))
+
+fetch_urls = [
+    f'http://mis.nyiso.com/public/csv/pal/{year:4}{month:02}01pal_csv.zip'
+    for year in years
+    for month in months
+]
+
+filenames = [
+    Path(f'fetch_nyiso_load/nyiso-{year}-{month}.zip')
+    for year in years
+    for month in months
+]
+
+for url, filename in zip(fetch_urls, filenames):
+    if not filename.is_file():
+        urlretrieve(url, filename=filename)
